59e7df702d7e2eb2921352d4eb18a17b0deea3ca
Jose <josemaurette1234@gmail.com>
2025-02-12 16:15:06 -0500

    Fixed Typo

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -23,7 +23,7 @@ const Projects = () => {
       image: "/portfolio.svg",
       href: "https://github.com/JoseMaurette1",
       description:
-        "Personal Dev Portfolio to shocwase Projects, Skills and Resume.",
+        "Personal Dev Portfolio to showcase Projects, Skills and Resume.",
       stack: [
         "/react.svg",
         "/nextjs.svg",
@@ -56,15 +56,12 @@ const Projects = () => {
       <h1 className="text-3xl">Projects</h1>
       <div className="pb-4 pt-8 w-full flex flex-col items-center space-y-8">
         {projectsinfo.map((project, index) => (
-          <div
-            key={index}
-            className="flex flex-row items-center space-x-8" // Flex row layout with spacing
-          >
+          <div key={index} className="flex flex-row items-center space-x-8">
             {/* Project Box */}
             <Box
               sx={{
-                width: 200, // Adjusted width
-                height: 200, // Adjusted height
+                width: 300, // Adjusted width
+                height: 300, // Adjusted height
                 backgroundColor: "#434343",
                 display: "flex",
                 flexDirection: "column",
@@ -92,8 +89,8 @@ const Projects = () => {
                 <Image
                   src={project.image}
                   alt={project.name}
-                  width={80} // Adjusted image size
-                  height={80}
+                  width={150}
+                  height={150}
                 />
                 <h1 className="text-2xl pt-2">{project.name}</h1>
               </a>
@@ -101,9 +98,10 @@ const Projects = () => {
 
             {/* Project Stack and Description */}
             <div className="flex flex-col space-y-4 max-w-md">
+              <p className="text-xl">{project.description}</p>
+              <h2 className="text-xl font-semibold">Tech Stack:</h2>
               <div>
-                <h2 className="text-xl font-semibold">Tech Stack</h2>
-                <div className="flex flex-wrap space-x-3">
+                <div className="flex flex-wrap space-x-3 ">
                   {project.stack.map((image, index) => (
                     <Image
                       key={index}
@@ -111,14 +109,12 @@ const Projects = () => {
                       alt="Tech Stack"
                       width={50}
                       height={50}
+                      className="transition duration-200 hover:-translate-y-1"
                     />
                   ))}
                 </div>
               </div>
-              <div>
-                <h2 className="text-xl font-semibold">Description</h2>
-                <p className="text-lg">{project.description}</p>
-              </div>
+              <div></div>
             </div>
           </div>
         ))}
